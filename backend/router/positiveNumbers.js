aef6e5ad7d7fa7f58b9ee49d672ef5e0d45a990d
Corrado Dibattista <corradodiba@gmail.com>
2019-10-13 18:33:17 +0200

    added avg to positive numbers exercise

--- backend/router/positiveNumbers.js
+++ backend/router/positiveNumbers.js
@@ -4,18 +4,22 @@ const router = express.Router();
 
 router.use("/:number", (req, res, next) => {
     
-    range = !req.query.range ? 5000 : parseInt(req.query.range);
+    range = req.query.range === undefined ? 5000 : req.query.range;
     positiveNumbers = [];
+    sum = 0;
+
     if (req.params.number >= 0) {
         for (let i = req.params.number; i <= range; i++) {
             if (i % 2 === 0) {
                 positiveNumbers.push(i);
+                sum += i;
             }
         }
         res.json({
             start: req.params.number,
             maxSafe: range,
-            numbers: positiveNumbers
+            numbers: positiveNumbers,
+            avg: sum / positiveNumbers.length
         });
     } else {
         res.json({
